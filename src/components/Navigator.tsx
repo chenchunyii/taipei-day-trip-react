97c7753abdb16f56ae0e0c6246a9f2c0df392ff7
chun <chun.yii.chen@gmail.com>
2025-05-09 02:47:05 +0800

    modify navigator bar

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,15 +1,17 @@
 const Navigator = () => {
   return (
     <>
-      <nav className="navigator display_flex">
-        <a className="navigator_title" href="/">
-          台北一日遊
-        </a>
-        <div className="navigator_links">
-          <div className="navigator_book">預定行程</div>
-          <div className="navigator_singup">登入/註冊</div>
-        </div>
-      </nav>
+      <div className="navigator_bg">
+        <nav className="navigator display_flex">
+          <a className="navigator_title" href="/">
+            台北一日遊
+          </a>
+          <div className="navigator_links">
+            <div className="navigator_book">預定行程</div>
+            <div className="navigator_singup">登入/註冊</div>
+          </div>
+        </nav>
+      </div>
       <nav className="nv-bg" />
     </>
   );
